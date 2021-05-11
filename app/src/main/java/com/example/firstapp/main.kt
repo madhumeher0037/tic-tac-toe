fa67c13b0223708ddae8a558eface658559e1ed2
madhumeher0037 <70276971+madhumeher0037@users.noreply.github.com>
2021-05-11 20:18:19 +0530

    kotlin2021

--- app/src/main/java/com/example/firstapp/main.kt
+++ app/src/main/java/com/example/firstapp/main.kt
@@ -1,5 +1,9 @@
 package com.example.firstapp
 
 fun main() {
-    print("hello world")
+    var my_name : String = "android kotlin"
+    var my_float : Float= 13.37f
+    var my_double : Double  = 3.121526358979
+    var my_int : Int = 25
+    print("$my_name\n$my_float\n$my_double\n$my_int")
 }
